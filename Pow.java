61a964ec6c7b8d647ec133308aa1a9863588cd6d
divee <diveesubburaj@gmail.com>
2016-07-21 15:03:10 +0530

    Update and rename Power.java to Pow.java

---
+++ Pow.java
@@ -0,0 +1,13 @@
+class Pow{
+public static void main(String angt[]){
+     Scanner data = new Scanner(System.in);
+      int num1, num2;
+      double num;
+      System.out.println("Enter a number");
+      num1=data.nextInt();
+      System.out.println("Enter a power of the number");
+      num2=data.nextInt();
+      num=Math.pow(num1,num2);
+      System.out.println("Answer:"+num);
+  }
+}

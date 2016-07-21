79a3df4f68556332faa1de5511126d16f1359571
divee <diveesubburaj@gmail.com>
2016-07-21 10:52:19 +0530

    Update and rename positive.java to Positive.java

---
+++ Positive.java
@@ -0,0 +1,22 @@
+# divya
+import java.util.*;
+public class Positive{
+public static void main(String args[0]){
+Scanner s=new Scanner(System.in);
+       Syatem.out.println("enter the number:")
+       int a=s.nextInt();
+       if(a>0)
+       {
+       System.out.println("it is a positive number");
+       }
+       else if(a<0)
+       {
+       System.out.println("it is a negative number");
+       }
+       else
+       {
+       System.out.println("it is zero");
+       }
+       }
+       }
+       

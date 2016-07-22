d3ba4ae582d2aeaf2e5b2f21bc3e1d8a3e428f57
divee <diveesubburaj@gmail.com>
2016-07-22 15:13:55 +0530

    Create Working.java

---
+++ Working.java
@@ -0,0 +1,25 @@
+class Working{
+  public static void main(String args[]){
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  switch(str){
+case monday:{
+     return true;
+     }
+     case tuesday:{
+     return true;
+     }
+     case wednesday:{
+     return true;
+     }
+     case thursday:{
+     return true;
+     }
+     case friday:{
+     return true;
+     }
+     default:return false;
+     }
+  }
+}
+     

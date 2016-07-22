f77241b11bbfed697e3e41a3ae94dbf85c5979b0
divee <diveesubburaj@gmail.com>
2016-07-22 09:14:58 +0530

    Create StrRev.java

---
+++ StrRev.java
@@ -0,0 +1,12 @@
+public class StrRev{
+public static void main(String args[]){
+ String str="mdsf";
+        StringBuffer a;
+        String b;
+        StringBuffer sb;
+        sb=new StringBuffer(str);
+        a=sb.reverse();
+        b=sb.toString();
+        System.out.println(b);
+        }
+        }
